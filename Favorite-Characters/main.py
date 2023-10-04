53592369a42e74669dc47dffcde4b041b9db0ed3
Sachin Karki <88551816+sachin-karki@users.noreply.github.com>
2023-10-04 15:30:38 -0500

    Add files via upload

---
+++ Favorite-Characters/main.py
@@ -0,0 +1,12 @@
+# Sachin Karki
+# Purpose: Assignment 4, Using for control function print a list of characters
+
+#Creating a list of characters
+myCharacters = ['Rango', 'Travis Bickle', 'Michael Corleone', 'Jack Reacher']
+
+#Printing each character and specifying a favorite one
+for character in myCharacters:
+  if character == 'Travis Bickle':
+    print(character, "is my favorite")
+  else:
+    print(character)
